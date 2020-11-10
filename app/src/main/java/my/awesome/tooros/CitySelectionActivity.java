4cf553059f159a39763febcf88413e9a97da0ec8
amanmishra086 <amanm1408@gmail.com>
2020-11-10 12:00:11 +0530

    main screen layout added

--- app/src/main/java/my/awesome/tooros/CitySelectionActivity.java
+++ app/src/main/java/my/awesome/tooros/CitySelectionActivity.java
@@ -6,21 +6,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class CitySelectionActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     androidx.appcompat.widget.Toolbar toolbar;
+
+    Calendar myEndCalendar;
+    Calendar myStartCalendar;
+    TextView startdate,enddate;
+    DatePickerDialog.OnDateSetListener startdatelistener;
+    DatePickerDialog.OnDateSetListener enddatelistener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +47,14 @@ public class CitySelectionActivity extends AppCompatActivity implements Navigati
         navigationView=findViewById(R.id.nav_view);
         toolbar=findViewById(R.id.toolbar);
 
+        startdate=findViewById(R.id.startDate);
+        enddate=findViewById(R.id.endDate);
+
         setSupportActionBar(toolbar);
 
         //hide or show items
       Menu menu = navigationView.getMenu();
-        menu.findItem(R.id.nav_logout).setVisible(false);
+      menu.findItem(R.id.nav_logout).setVisible(false);
 
 
         navigationView.setItemIconTintList(null);
@@ -53,7 +72,85 @@ public class CitySelectionActivity extends AppCompatActivity implements Navigati
         navigationView.setCheckedItem(R.id.nav_home);
 
 
+
+         myStartCalendar = Calendar.getInstance();
+         myEndCalendar = Calendar.getInstance();
+
+
+         startdatelistener = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myStartCalendar.set(Calendar.YEAR, year);
+                myStartCalendar.set(Calendar.MONTH, monthOfYear);
+                myStartCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                String myFormat = "dd/MM/yy"; //In which you need put here
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                String dateSelected=sdf.format(myStartCalendar.getTime());
+
+                startdate.setText(dateSelected);
+
+
+               // Toast.makeText(CitySelectionActivity.this,sdf.format(myStartCalendar.getTime()) , Toast.LENGTH_SHORT).show();
+            }
+
+        };
+        enddatelistener = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myEndCalendar.set(Calendar.YEAR, year);
+                myEndCalendar.set(Calendar.MONTH, monthOfYear);
+                myEndCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                String myFormat = "dd/MM/yy"; //In which you need put here
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                String dateSelected=sdf.format(myStartCalendar.getTime());
+
+                enddate.setText(dateSelected);
+
+                //Toast.makeText(CitySelectionActivity.this,sdf.format(myEndCalendar.getTime()) , Toast.LENGTH_SHORT).show();
+            }
+
+        };
+
+
     }
+    private void updateLabel() {
+        String myFormat = "dd/MM/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        //Toast.makeText(this,sdf.format(myCalendar.getTime()) , Toast.LENGTH_SHORT).show();
+
+    }
+    public void onClickStartDate(View view) {
+
+        new DatePickerDialog(CitySelectionActivity.this, startdatelistener, myStartCalendar
+                .get(Calendar.YEAR), myStartCalendar.get(Calendar.MONTH),
+                myStartCalendar.get(Calendar.DAY_OF_MONTH)).show();
+
+
+    }
+
+    public void onClickEndDate(View view) {
+        new DatePickerDialog(CitySelectionActivity.this, enddatelistener, myEndCalendar
+                .get(Calendar.YEAR), myEndCalendar.get(Calendar.MONTH),
+                myEndCalendar.get(Calendar.DAY_OF_MONTH)).show();
+
+    }
+    public void onClickFindCarButton(View view) {
+        Toast.makeText(this, "No Car Found !!", Toast.LENGTH_SHORT).show();
+
+    }
+
+
 
     @Override
     public void onBackPressed(){
@@ -86,4 +183,7 @@ public class CitySelectionActivity extends AppCompatActivity implements Navigati
 
         return true;
     }
+
+
+
 }
